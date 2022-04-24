6c3e8a3290a856cf7e215a7c3b518bc57a998260
Khanh Tran <ledgechan@gmail.com>
2022-04-24 17:11:36 +0700

    Part-17

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import URLPattern, path
 from . import views
 
+
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
@@ -8,6 +9,11 @@ urlpatterns = [
     
     path('', views.home, name="home"),
     path('user/', views.userPage, name="user-page"),
+
+
+    path('account', views.accountSettings, name="account"),
+
+
     path('products/', views.products, name="products"),
     path('customer/<str:pk>/', views.customer, name="customer"),
 
